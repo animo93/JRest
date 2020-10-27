11412c1e589360c56ff91739f121e92c27ce590e
animo <animeshmohanty84@gmail.com>
2020-10-27 14:45:42 +0530

    First commit for bracn Headers

--- src/main/java/com/animo/jRest/annotation/HEADERS.java
+++ src/main/java/com/animo/jRest/annotation/HEADERS.java
@@ -1,13 +1,16 @@
 package com.animo.jRest.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author animo
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface HEADERS {
 	String[] value();
 }
