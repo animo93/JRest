d70513204546327430e44207290a39b577b12175
3looy-hubs <Alihussainkhamis91@gmail.com>
2024-10-17 09:39:30 +0300

    deleted IThrowingFunction interface

--- src/main/java/com/animo/jRest/exception/IThrowingFunction.java
+++
@@ -1,9 +0,0 @@
-package com.animo.jRest.exception;
-
-import java.util.concurrent.ExecutionException;
-
-@FunctionalInterface
-public interface IThrowingFunction<T, R> {
-    R apply(T t) throws InterruptedException, ExecutionException;
-}
-
